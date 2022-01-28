22e9a1cbdd454078b6f571b9ae39f6b68ff65537
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2022-01-29 01:40:54 +0530

    Animation Added

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import loadingGif from '../Images/loading-gif.gif'
 
 const Loading = () => {
   return( <div className='form-loading' >
-      
+      <img width="170px" src={loadingGif}/>
   </div>)
 };
 
