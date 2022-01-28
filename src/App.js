2dce62cce31801f0eea7ae5cae0dc04a1a909dec
Om Prakash <71120474+OmPr366@users.noreply.github.com>
2022-01-29 01:51:15 +0530

    LocalStorage Saved

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TextCard from "./Components/TextCard";
 import ToggleCard from "./Components/ToggleCard";
 import Button from "@mui/material/Button";
 import "./CSS/App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CheckCard from "./Components/CheckCard";
 import Alert from "@mui/material/Alert";
 import fetch from "isomorphic-fetch";
@@ -18,8 +18,9 @@ function App() {
     discord: "",
   });
   const [isAlert, setIsAlert] = useState(false);
-  const [isSucess, setIsSucess] = useState(false);
+  const [isSucess, setIsSucess] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [isSubmit, setIsSubmit] = useState("");
 
   const submit = () => {
     setIsLoading(true);
@@ -72,6 +73,7 @@ function App() {
         }),
       })
         .then((response) => {
+          localStorage.setItem("gfgreg", "true");
           setIsSucess(true);
           setIsLoading(false);
         })
@@ -88,13 +90,16 @@ function App() {
     console.log(parsedData);
   };
 
+  // Checking Is Submit
+
+  useEffect(() => {
+    if (localStorage.getItem("gfgreg")) {
+      setIsSubmit(true);
+    }
+  }, [isLoading]);
+
   return (
-    <div className="app-js">
-      <div id="form-alert" className={isAlert ? "" : "form-hidden"}>
-        <Alert className="form-alert-box" severity="error">
-          Please fill all the fields
-        </Alert>
-      </div>
+    <>
       <div id="form-sucess" className={isSucess ? "" : "form-hidden"}>
         <Alert
           onClose={() => {
@@ -104,53 +109,67 @@ function App() {
           Thanks for your submission!
         </Alert>
       </div>
-      <TextCard
-        myId="name"
-        title={"Name *"}
-        value={inputVal}
-        setInputVal={setInputVal}
-        name="name"
-      />
-      <TextCard
-        myId="email"
-        title={"Email *"}
-        value={inputVal}
-        setInputVal={setInputVal}
-        name="email"
-      />
-      <ToggleCard
-        myId="year"
-        title={"Year"}
-        value={inputVal}
-        setInputVal={setInputVal}
-        name="year"
-      />
-      <CheckCard
-        myId="domain"
-        title={"Domain"}
-        value={inputVal}
-        setInputVal={setInputVal}
-        name="domain"
-      />
-      <TextCard
-        myId="discord"
-        title={
-          "Discord ID: ( ex: abcdg#1234) (In case you don't have a discord account, please make one. We will be communicating via discord for all team related activities.)"
-        }
-        value={inputVal}
-        setInputVal={setInputVal}
-        name="discord"
-      />
-      <div className="form-submit-button">
-        <Button onClick={submit} variant="contained" color="success">
-          Submit
-        </Button>
-        <Button onClick={getData} variant="contained" color="success">
-          Get Data
-        </Button>
-      </div>
-      {isLoading ? <Loading /> : ""}
-    </div>
+      {!isSubmit ? (
+        <div className="app-js">
+          <div id="form-alert" className={isAlert ? "" : "form-hidden"}>
+            <Alert className="form-alert-box" severity="error">
+              Please fill all the fields
+            </Alert>
+          </div>
+
+          <TextCard
+            myId="name"
+            title={"Name *"}
+            value={inputVal}
+            setInputVal={setInputVal}
+            name="name"
+          />
+          <TextCard
+            myId="email"
+            title={"Email *"}
+            value={inputVal}
+            setInputVal={setInputVal}
+            name="email"
+          />
+          <ToggleCard
+            myId="year"
+            title={"Year"}
+            value={inputVal}
+            setInputVal={setInputVal}
+            name="year"
+          />
+          <CheckCard
+            myId="domain"
+            title={"Domain"}
+            value={inputVal}
+            setInputVal={setInputVal}
+            name="domain"
+          />
+          <TextCard
+            myId="discord"
+            title={
+              "Discord ID: ( ex: abcdg#1234) (In case you don't have a discord account, please make one. We will be communicating via discord for all team related activities.)"
+            }
+            value={inputVal}
+            setInputVal={setInputVal}
+            name="discord"
+          />
+          <div className="form-submit-button">
+            <Button onClick={submit} variant="contained" color="success">
+              Submit
+            </Button>
+            <Button onClick={getData} variant="contained" color="success">
+              Get Data
+            </Button>
+          </div>
+          {isLoading ? <Loading /> : ""}
+        </div>
+      ) : (
+        <>
+          <div>Already Submitted</div>
+        </>
+      )}
+    </>
   );
 }
 
