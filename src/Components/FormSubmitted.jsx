23531d92e70a1715e7326a8fc3ce0bcec1b87081
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2022-01-29 14:19:32 +0530

    updated

--- src/Components/FormSubmitted.jsx
+++ src/Components/FormSubmitted.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import loadingGif from '../Images/form-submitted-gif.gif';
 const FormSubmitted = () => {
-  return(
+  return (
     <>
-        <img width="350px" src={loadingGif}/>
+      <div className="form-submitted">
+        <img className="image-unselectable" width="350px" src={loadingGif} />
+        <h2>Form submitted sucessfully, you can close this window now!</h2>
+      </div>
     </>
   );
 };
--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import loadingGif from '../Images/loading-gif.gif'
 
 const Loading = () => {
-  return( <div className='form-loading' >
-      <img width="170px" src={loadingGif}/>
-  </div>)
+  return (
+    <div className="form-loading">
+      <img className="image-unselectable" width="170px" src={loadingGif} />
+    </div>
+  );
 };
 
 export default Loading;
