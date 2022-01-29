95ba12c6f12141d350f490a27b446ab58fac04d5
Himanshu Mishra <66140981+himanshu070@users.noreply.github.com>
2022-01-29 14:43:04 +0530

    Form info component added

--- src/Components/FormInfo.jsx
+++ src/Components/FormInfo.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
 
-const FormInfo = () => {
-  return <></>;
+const FormInfo = ({title, content}) => {
+  return (
+    <>
+      <div id="card-div">
+        <Card id="card" sx={{ maxWidth: 345 }}>
+          <CardContent>
+            <Typography
+              className="card-title"
+              gutterBottom
+              variant="h5"
+              component="div"
+              fontSize="37px"
+            >
+              {title}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              fontSize="15px"
+            >
+              {content}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default FormInfo;
